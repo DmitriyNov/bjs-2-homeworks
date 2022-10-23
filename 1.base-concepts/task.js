a56e7dc9ace8613ad294e068f666f44df05111f4
Dmitriy Novokreschenov <NovokrDm@yandex.ru>
2022-10-23 21:23:26 +0300

    Task completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,47 @@
+'use strict';
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let discr = Math.pow(b, 2) - 4 * a * c;
+  if (discr < 0) {
+    arr = [];
+  } else if (discr > 0) {
+    arr = [(-b + Math.sqrt(discr) )/(2*a), (-b - Math.sqrt(discr) )/(2*a)];
+  } else {
+    arr = [-b/(2*a)];
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  //проверка типа данных
+  if (isNaN(percent)) {
+    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    console.log(totalAmount);
+    return totalAmount;
+  } else percent = +percent;
+  if (isNaN(contribution)) {
+    totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    console.log(totalAmount);
+    return totalAmount;
+  } else contribution = +contribution;
+  if (isNaN(amount)) {
+    totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+    console.log(totalAmount);
+    return totalAmount;
+  } else amount = +amount;
+  //тело кредита
+  let body = amount - contribution;
+  //срок кредита (вообще не понял, как правильно считать, работает, когда округляешь в меньшую сторону)
+  date = Date.parse(date);
+  const current = Date.now();
+  let term = Math.floor((date - current) / 2592000000);
+  //ежемесячный платёж
+  let monthly = body * ((percent / 100 / 12) + ((percent / 100 / 12) / ((((1 + (percent / 100 / 12)) ** term) - 1))))
+  console.log(monthly);
+  //сумма платежа
+  totalAmount = monthly * term;
+  totalAmount = +totalAmount.toFixed(2);
   return totalAmount;
 }
