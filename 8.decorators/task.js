820831845ec34bb1b37044086f593ac53da02551
Dmitriy Novokreschenov <NovokrDm@yandex.ru>
2022-11-20 18:29:44 +0300

    Task completed

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,47 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  return (...args) => {
+    const hash = args.join(",");
+    if(cache.some((item) => {
+      return item.hash === hash;
+    })) {
+      let index = cache.findIndex((item) => {
+        return item.hash === hash;
+      });
+
+      console.log("Из кэша: " + cache[index].value);
+      return "Из кэша: " + cache[index].value;
+    }
+    const result = func(...args);
+    if (cache.length >= 5) {
+      cache.shift();
+    }
+    cache.push({hash: hash, value: result});
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  function wraper(...args) {
+    wraper.allCount += 1;
+    if(timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    if(timeoutId === null) {
+      timeoutId = setTimeout(() => {
+        wraper.count += 1;
+        return func(...args);
+      }, 0);
+    }
+    timeoutId = setTimeout(() => {
+      wraper.count += 1;
+      return func(...args);
+    }, delay);
+  }
+  wraper.count = 0;
+  wraper.allCount = 0;
+  return wraper;
 }
